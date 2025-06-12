96a11405463eed07abdad576ba9851808135e856
Matt Archer <ma595@cam.ac.uk>
2025-06-12 10:28:41 +0100

    Update to epoch 85 model

--- era5_training/get-model-and-data.sh
+++ era5_training/get-model-and-data.sh
@@ -5,12 +5,12 @@ mkdir -p inputs
 
 echo "retrieving model weights..."
 cd model-huggingface
-wget https://huggingface.co/amangupta2/iccs_coupling_checkpoints/resolve/main/retrained_ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch45.pt
+wget https://huggingface.co/amangupta2/iccs_coupling_checkpoints/resolve/main/retrained_ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch85.pt
 wget https://huggingface.co/amangupta2/iccs_coupling_checkpoints/resolve/main/ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch94.pt
 wget https://huggingface.co/amangupta2/iccs_coupling_checkpoints/resolve/main/attnunet_era5_global_global_uvthetaw_mseloss_train_epoch119.pt
 cd ..
 
-mv model-huggingface/retrained_ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch45.pt model-huggingface/ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch45.pt
+mv model-huggingface/retrained_ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch85.pt model-huggingface/ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch85.pt
 
 echo "retrieving test input..."
 (cd inputs && wget https://g-b56e81.7a577b.6fbd.data.globus.org/1x1_inputfeatures_u_v_theta_w_uw_vw_era5_training_data_hourly_2015_constant_mu_sigma_scaling01.nc)
