552df3b1dc0ba860cdcfde57dd40e633c8c37dd4
tommelt <thomas.meltzer1@gmail.com>
2026-02-02 18:36:55 -0700

    chore: update input data file

--- era5_training/get-model-and-data.sh
+++ era5_training/get-model-and-data.sh
@@ -13,4 +13,4 @@ cd ..
 mv model-huggingface/retrained_ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch85.pt model-huggingface/ann_cnn_1x1_global_global_era5_uvthetaw__train_epoch85.pt
 
 echo "retrieving test input..."
-(cd inputs && wget https://g-b56e81.7a577b.6fbd.data.globus.org/1x1_inputfeatures_u_v_theta_w_uw_vw_era5_training_data_hourly_2015_constant_mu_sigma_scaling01.nc)
+(cd inputs && wget https://g-b56e81.7a577b.6fbd.data.globus.org/1x1_inputfeatures_u_v_theta_w_uw_vw_gcp_era5_training_data_hourly_2015_L93_constant_mu_sigma_scaling01.nc)
